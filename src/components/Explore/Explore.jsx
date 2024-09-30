2d1e8c8a6b8ed928d9878fac5c15021ed50cf68f
victoria <itsfoxiefo@gmail.com>
2024-09-30 18:31:33 +0200

    added animation to Explore

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -10,6 +10,7 @@ import VideoModal from '../ui/VideoModal/VideoModal';
 
 export default function Explore() {
     const [isVideoOpen, setIsVideoOpen] = useState(false);
+    const [activeImg, setActiveImg] = useState('village');
 
     const handleVideoOpen = () => {
         setIsVideoOpen(true);
@@ -19,28 +20,46 @@ export default function Explore() {
         setIsVideoOpen(false);
     };
 
+    const handleActiveImg = (image) => {
+        setActiveImg(image);
+    };
+
     return (
-            <section className={s.explore__container}>
-                <div className={s.explore__content}>
-                    <div className={s.explore__media}>
-                        <img className={s.explore__mediaImage} alt='Mountains' src={Mountains}>
-                        </img>
-                        <div className={s.explore__mediaVideo}>
-                            <img className={s.explore__mediaVideoImg} alt='Village' src={Village}>
-                            </img>
-                            <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
-                                <img src={PlayButton} alt="Play" />
-                            </button>
-                        </div>
+        <section className={s.explore__container}>
+            <div className={s.explore__content}>
+                <div className={s.explore__media}>
+                    <div
+                        className={`${s.explore__mediaVideo} ${activeImg === 'mountains' ? s.active : ''
+                            }`}
+                        onClick={() => handleActiveImg('mountains')}
+                    >
+                        <img className={s.explore__mediaVideoImg} alt="Mountains" src={Mountains} />
+                        <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
+                            <img src={PlayButton} alt="Play" />
+                        </button>
                     </div>
-                    <div className={s.explore__info}>
-                        <span className={s.explore__infoAbout}>about our tour</span>
-                        <h1 className={s.explore__infoExplore}>Explore all the mountain ranges of the world with us</h1>
-                        <p className={s.explore__infoText}>Join our groups for exciting hikes along scenic routes, where you’ll not only enjoy the beauty of nature but also meet like-minded people. Our experienced guides will ensure a safe and comfortable journey, share insights about local flora and fauna, and tell interesting stories about the regions we explore.</p>
-                        <Button title={'Tour program'} className={s.explore__infoButton}/>
+                    <div
+                        className={`${s.explore__mediaVideo} ${activeImg === 'village' ? s.active : ''
+                            }`}
+                        onClick={() => handleActiveImg('village')}
+                    >
+                        <img className={s.explore__mediaVideoImg} alt="Village" src={Village} />
+                        <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
+                            <img src={PlayButton} alt="Play" />
+                        </button>
                     </div>
-                    <VideoModal isOpen={isVideoOpen} onClose={handleVideoClose}/>
                 </div>
-            </section>
-        );
+                <div className={s.explore__info}>
+                    <span className={s.explore__infoAbout}>about our tour</span>
+                    <h1 className={s.explore__infoExplore}>
+                        Explore all the mountain ranges of the world with us
+                    </h1>
+                    <p className={s.explore__infoText}> Join our groups for exciting hikes along scenic routes, where you’ll not only enjoy the beauty of nature but also meet like-minded people. Our experienced guides will ensure a safe and comfortable journey, share insights about local flora and fauna, and tell interesting stories about the regions we explore.
+                    </p>
+                    <Button title={'Tour program'} className={s.explore__infoButton} />
+                </div>
+                <VideoModal isOpen={isVideoOpen} onClose={handleVideoClose} />
+            </div>
+        </section>
+    );
 }

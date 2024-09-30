83e057bdae41170636d7d759c27240e38c2c72c1
victoria <itsfoxiefo@gmail.com>
2024-09-30 20:32:07 +0200

    Explore: tablet/mobile — ready

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -28,31 +28,30 @@ export default function Explore() {
         <section className={s.explore__container}>
             <div className={s.explore__content}>
                 <div className={s.explore__media}>
-                    <div
-                        className={`${s.explore__mediaVideo} ${activeImg === 'mountains' ? s.active : ''
-                            }`}
-                        onClick={() => handleActiveImg('mountains')}
-                    >
-                        <img className={s.explore__mediaVideoImg} alt="Mountains" src={Mountains} />
-                        <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
-                            <img src={PlayButton} alt="Play" />
-                        </button>
+                    <div className={s.explore__mediaDesktop}>
+                        <div className={`${s.explore__mediaVideo} ${activeImg === 'mountains' ? s.active : ''}`} onClick={() => handleActiveImg('mountains')}>
+                            <img className={s.explore__mediaVideoImg} alt="Mountains" src={Mountains} />
+                            <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
+                                <img src={PlayButton} alt="Play" />
+                            </button>
+                        </div>
+                        <div className={`${s.explore__mediaVideo} ${activeImg === 'village' ? s.active : ''}`} onClick={() => handleActiveImg('village')}>
+                            <img className={s.explore__mediaVideoImg} alt="Village" src={Village} />
+                            <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
+                                <img src={PlayButton} alt="Play" />
+                            </button>
+                        </div>
                     </div>
-                    <div
-                        className={`${s.explore__mediaVideo} ${activeImg === 'village' ? s.active : ''
-                            }`}
-                        onClick={() => handleActiveImg('village')}
-                    >
-                        <img className={s.explore__mediaVideoImg} alt="Village" src={Village} />
-                        <button className={s.explore__mediaVideoBtn} onClick={handleVideoOpen}>
+                    <div className={s.explore__mediaMob}>
+                        <img className={s.explore__mediaMobImg} alt="Village" src={Village} />
+                        <button className={s.explore__mediaMobBtn} onClick={handleVideoOpen}>
                             <img src={PlayButton} alt="Play" />
                         </button>
                     </div>
                 </div>
                 <div className={s.explore__info}>
                     <span className={s.explore__infoAbout}>about our tour</span>
-                    <h1 className={s.explore__infoExplore}>
-                        Explore all the mountain ranges of the world with us
+                    <h1 className={s.explore__infoExplore}> Explore all the mountain ranges of the world with us
                     </h1>
                     <p className={s.explore__infoText}> Join our groups for exciting hikes along scenic routes, where you’ll not only enjoy the beauty of nature but also meet like-minded people. Our experienced guides will ensure a safe and comfortable journey, share insights about local flora and fauna, and tell interesting stories about the regions we explore.
                     </p>

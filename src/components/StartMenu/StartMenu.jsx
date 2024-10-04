d13f2adf58043c64e32343bbfbbbafec0c807c47
victoria <itsfoxiefo@gmail.com>
2024-10-04 18:01:30 +0200

    StartMenu: tablet/mobile — ready

--- src/components/StartMenu/StartMenu.jsx
+++ src/components/StartMenu/StartMenu.jsx
@@ -1,6 +1,7 @@
 import s from './styles.module.scss';
 import Header from '../Header/Header';
 import FindRoute from '../ui/FindRoute/FindRoute';
+import Button from '../ui/Button/Button';
 
 export default function StartMenu() {
     return (
@@ -10,6 +11,7 @@ export default function StartMenu() {
                 <div className={s.start__menu}>
                     <p className={s.start__menuText}>Enjoy a mountain hike with a group of like-minded adventurers</p>
                     <FindRoute />
+                    <Button className={s.start__menuButton} title={'Find a program'} colorScheme='colorScheme__2'/>
                 </div>
             </div>
         </section>
--- src/components/ui/FindRoute/FindRoute.jsx
+++ src/components/ui/FindRoute/FindRoute.jsx
@@ -9,10 +9,10 @@ export default function FindRoute() {
     return (
         <>
             <div className={s.findroute__container}>
-            <DropDown title={'Tour location'} subtitle={'select from the list'} options={locations}/>
-<DropDown title={'Hiking date'} subtitle={'specify the range'} calendar={true}/>
-<DropDown title={'Participants'} subtitle={'minimum of 4 people'} options={participants}/>
-<Button title={'Find a program'} colorScheme='colorScheme__2' className={s.findroute__button}/>
+                <DropDown title={'Tour location'} subtitle={'select from the list'} options={locations} />
+                <DropDown title={'Hiking date'} subtitle={'specify the range'} calendar={true} />
+                <DropDown title={'Participants'} subtitle={'minimum of 4 people'} options={participants} />
+                <Button title={'Find a program'} colorScheme='colorScheme__2' className={s.findroute__button} />
             </div>
         </>
     );

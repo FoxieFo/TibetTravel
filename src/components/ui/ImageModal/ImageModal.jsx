0bf9e35b4090ed2b68c7fc842e89fc7e2277d521
victoria <itsfoxiefo@gmail.com>
2024-10-06 20:25:49 +0200

    changed slider in ImageModal

--- src/components/ui/ImageModal/ImageModal.jsx
+++ src/components/ui/ImageModal/ImageModal.jsx
@@ -1,37 +1,51 @@
 import s from './styles.module.scss';
-import { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Pagination, Navigation } from 'swiper/modules';
 
-import Next from './../../../assets/images/icons/next.svg'
-import Prev from './../../../assets/images/icons/prev.svg'
-
-export default function ImageModal ({ isOpen, onClose, images, currentIndex }) {
-    const [currentIndexState, setCurrentIndexState] = useState(currentIndex);
-
-    useEffect(() => {
-        setCurrentIndexState(currentIndex);
-    }, [currentIndex]);
-
+export default function ImageModal({ isOpen, onClose, images, currentIndex }) {
     if (!isOpen) return null;
 
-    const handlePrev = () => {
-        setCurrentIndexState((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-    };
-
-    const handleNext = () => {
-        setCurrentIndexState((prevIndex) => (prevIndex + 1) % images.length);
-    };
+    const CloseIcon = ({ onClick }) => (
+        <div className={s.imagemodal__closebutton} onClick={onClick}>
+            <svg width="34" height="34" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18 6L6 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M6 6L18 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        </div>
+    );
 
     return (
         <div className={s.imagemodal}>
             <div className={s.imagemodal__overlay} onClick={onClose}></div>
             <div className={s.imagemodal__content}>
-                <button className={s.imagemodal__closebutton} onClick={onClose}>
-                    close ✖
-                </button>
+                <CloseIcon onClick={onClose} />
                 <div className={s.imagemodal__modalinner}>
-                    <button className={s.imagemodal__modalinnerPrev} onClick={handlePrev}></button>
-                    <img src={images[currentIndexState]} className={s.imagemodal__modalinnerImage}/>
-                    <button className={s.imagemodal__modalinnerNext} onClick={handleNext}></button>
+                    <Swiper
+                        slidesPerView={1}
+                        style={{
+                            '--swiper-navigation-color': '#fff',
+                            '--swiper-pagination-color': '#fff',
+                        }}
+                        loop={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className={s.imagemodal__swiper}
+                        initialSlide={currentIndex}
+                        centeredSlides= {true}
+                        centeredSlidesBounds= {true}
+                    >
+                        {images.map((image, index) => (
+                            <SwiperSlide className={s.imagemodal__swiperSlide} key={index}>
+                                <img src={image} alt={`Slide ${index}`} />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>

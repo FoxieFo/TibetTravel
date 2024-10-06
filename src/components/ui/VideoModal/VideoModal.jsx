147e6a66233f9eae1dd5d27d1978cf390affb1a3
victoria <itsfoxiefo@gmail.com>
2024-10-06 20:35:33 +0200

    tuned CloseButton in VideoModal

--- src/components/ui/VideoModal/VideoModal.jsx
+++ src/components/ui/VideoModal/VideoModal.jsx
@@ -1,5 +1,14 @@
 import s from './styles.module.scss';
 
+const CloseIcon = ({ onClick }) => (
+    <div className={s.videomodal__closebutton} onClick={onClick}>
+        <svg width="34" height="34" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 6L6 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M6 6L18 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    </div>
+);
+
 export default function VideoModal ({ isOpen, onClose }) {
     if (!isOpen) return null;
 
@@ -7,9 +16,7 @@ export default function VideoModal ({ isOpen, onClose }) {
         <div className={s.videomodal}>
             <div className={s.videomodal__overlay} onClick={onClose}></div>
             <div className={s.videomodal__content}>
-                <button className={s.videomodal__closebutton} onClick={onClose}>
-                    ✖
-                </button>
+                <CloseIcon onClick={onClose}/>
                 <iframe
                     src="https://www.youtube.com/embed/bBRgYIvaL00?autoplay=1"
                     title="YouTube video player"
